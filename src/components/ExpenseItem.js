7debfcc0305eb580fc2ec0562cb5c4ea938a1abb
sukku-akky <somasukku@gmail.com>
2024-08-28 21:52:20 +0530

    javascript inside jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,15 +2,20 @@ import React from "react";
 import "./ExpenseItem.css";
 
 const ExpenseItem=()=>{
+
+    const expenseDate=new Date(2023 ,3,22).toISOString();
+    const expenseTitle="Books";
+    const expensePrice=50;
+    const expenseLocation="Bangalore";
     return (
         <div className="expense-item">
-            <div>15 August 2023</div>
+            <div>{expenseDate}</div>
             <div className="expense-item__description">
-                <h2>Books</h2>
+                <h2>{expenseTitle}</h2>
             </div>
-            <div className="expense-item__price">$10</div>
+            <div className="expense-item__price">{expensePrice}</div>
             <div className="expense-item__location">
-                <h2>Delhi</h2>
+                <h2>{expenseLocation}</h2>
             </div>
 
         </div>
